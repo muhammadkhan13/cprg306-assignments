5f979dcb053ede6c3fe51507625a58b08f3990b7
muhammadkhan13 <muhammad.khan13@edu.sait.ca>
2024-09-24 19:04:07 -0600

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+    return(
+        <div>
+            <p>Name: Muhammad Khan</p>
+            <nav>
+                <Link href="https://github.com/muhammadkhan13/cprg306-assignments">Github Repository</Link>
+            </nav>
+        </div>
+    )
+}
